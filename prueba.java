6e6e433c3cbcade58724f5434eb7cf3fdd2e6277
tijedc <tijera19967@gmail.com>
2019-10-03 22:26:42 +0200

    Ahora muestra mi lugar de residencia actual

--- prueba.java
+++ prueba.java
@@ -2,5 +2,6 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Adrian Tijera De Celis");
 		System.out.println("Leon");
+		System.out.println("Villaquilambre");
 	}
 }
